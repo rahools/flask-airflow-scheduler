74911243c19ab6abca2842b102cd26219349deda
Rahul Singh <contact@rahools.com>
2021-07-29 02:03:21 +0530

    seperating starter bash scripts

--- start.sh
+++
@@ -1,5 +0,0 @@
-export AIRFLOW_HOME=/home/rahools/codes-p/flask-airflow-scheduler/airflow_home
-pipenv run airflow webserver &
-pipenv run airflow scheduler &
-sudo docker start mongodb
-sudo docker start postgresql
---
+++ webStart.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export AIRFLOW_HOME=/home/rahools/codes-p/flask-airflow-scheduler/airflow_home
+pipenv run airflow webserver
